ba179cfef7a9f30363c8ed685fbd0045a22135d9
Traffox <traffox@protonmail.com>
2020-05-11 21:13:47 +0300

    add templates

--- polls/views.py
+++ polls/views.py
@@ -3,12 +3,14 @@ from django.http import HttpResponse
 from django import template
 from .models import Question, Test, Poll
 from django.views import generic
+from datetime import datetime
 
 from .models import Poll
 
 
 def indexpage(request):
     client_ip = request.META['REMOTE_ADDR']
+    clock = datetime.now()
 
     return render(
         request,
@@ -17,6 +19,7 @@ def indexpage(request):
             'header':'Заголовок основой странички',
             'body':'Тело основной странички',
             'ip': client_ip,
+            'time':clock,
         }
     )
 
